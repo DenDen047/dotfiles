ba8f05cf5505f6f67fa3cad5a16f965f18649ccd
DenDen047 <sh.mn.nat@gmail.com>
2020-05-23 18:28:01 +0900

    complete the centos.sh

--- setup_scripts/centos.sh
+++ setup_scripts/centos.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
 echo "update..."
-sudo apt update
-sudo apt upgrade -y
+sudo yum update
+sudo yum upgrade -y
 echo -e "\n"
-
 
 echo "install some softwares..."
-sudo apt install -y vim
-sudo apt install -y git
-sudo apt install -y zsh
-sudo apt install -y tmux
+sudo yum install -y vim
+sudo yum install -y git
+sudo yum install -y zsh
+sudo yum install -y tmux
 echo -e "\n"
 
-echo "install python environment"
-sudo apt install -y python3-pip
-sudo pip3 install virtualenv
-
-
-echo "git setting..." git config --global user.name "denden047"
-git config --global user.email "sh.mn.nat@gmail.com"
-git config --global core.editor 'vim -c "set fenc=utf-8"'	# set vim for default editer
-git config --global color.diff auto
-git config --global color.status auto
-git config --global color.branch auto
-git config --global core.precomposeunicode true
+echo "git setting..."
+git config --local user.name "denden047"
+git config --local user.email "sh.mn.nat@gmail.com"
+git config --local core.editor 'vim -c "set fenc=utf-8"'	# set vim for default editer
+git config --local color.diff auto
+git config --local color.status auto
+git config --local color.branch auto
 git config --local core.precomposeunicode true
-git config --global core.quotepath false
+git config --local core.precomposeunicode true
+git config --local core.quotepath false
 echo "=================="
 git config --list
 echo "=================="
@@ -42,13 +37,6 @@ echo -e "\n"
 # read check
 # ssh -T git@github.com
 # echo -e "\n"
-
-
-echo "git clone some directories..."
-cd $HOME
-git clone https://github.com/DenDen047/dotfiles.git
-cd ./dotfiles
-echo -e "\n"
 
 echo "create symbolic link..."
 ln -s ~/dotfiles/.vimrc ~/.vimrc
