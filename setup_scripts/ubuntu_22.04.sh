4f816033cd4d833af635233864ced702c83e53b8
DenDen047 <sh.mn.nat@gmail.com>
2025-06-25 12:35:38 +0900

    update

--- setup_scripts/ubuntu_22.04.sh
+++ setup_scripts/ubuntu_22.04.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 echo "update..."
-sudo snap refresh
+sudo apt update
 echo -e "\n"
 
 echo "install some softwares..."
-sudo snap install vim
-sudo snap install git
-sudo snap install tmux
-sudo apt-get install -y zsh
+sudo apt install -y vim
+sudo apt install -y git
+sudo apt install -y tmux
+sudo apt install -y zsh
 echo -e "\n"
 
-echo "git setting..."
-git config --local user.name "denden047"
+# echo "git setting..."
+git config --local user.name "DenDen047"
 git config --local user.email "sh.mn.nat@gmail.com"
-git config --local core.editor 'vim -c "set fenc=utf-8"'	# set vim for default editer
-git config --local color.diff auto
-git config --local color.status auto
-git config --local color.branch auto
-git config --local core.precomposeunicode true
-git config --local core.precomposeunicode true
-git config --local core.quotepath false
-echo "=================="
-git config --list
-echo "=================="
-echo -e "\n"
+# git config --local core.editor 'vim -c "set fenc=utf-8"'	# set vim for default editer
+# git config --local color.diff auto
+# git config --local color.status auto
+# git config --local color.branch auto
+# git config --local core.precomposeunicode true
+# git config --local core.precomposeunicode true
+# git config --local core.quotepath false
+# echo "=================="
+# git config --list
+# echo "=================="
+# echo -e "\n"
 
 echo "create symbolic link..."
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.ssh/config ~/.ssh/config
+# ln -s ~/dotfiles/.ssh/config ~/.ssh/config
 
 # ====== VIM =======
 rm -rf ~/dotfiles/.vim/bundle/
