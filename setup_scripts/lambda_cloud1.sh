429c0b10596ffa8a4d37952848b90dd793a62642
Naoya Muramatsu <sh.mn.nat@gmail.com>
2025-07-23 19:16:48 +0900

    Update lambda_cloud1.sh

--- setup_scripts/lambda_cloud1.sh
+++ setup_scripts/lambda_cloud1.sh
@@ -23,5 +23,5 @@ sudo usermod -aG docker $USER
 LAMBDA_REPO=$(mktemp) && \
 	wget -O${LAMBDA_REPO} https://lambdalabs.com/static/misc/lambda-stack-repo.deb && \
 	sudo dpkg -i ${LAMBDA_REPO} && rm -f ${LAMBDA_REPO} && \
-	sudo apt-get update && sudo apt-get install -y lambda-stack-cuda
+	sudo apt-get update && sudo apt-get install -y lambda-stack-cuda && \
 sudo reboot
